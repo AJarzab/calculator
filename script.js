7b9ab924546ebb8657f106f4c464ef521bb18568
Amadeusz Jarzab <amadeuszjarzab@gmail.com>
2022-09-30 15:56:19 +0200

    Fixed floating point precision, reformated code

--- script.js
+++ script.js
@@ -13,86 +13,96 @@ function updateDisplay() {
 }
 
 updateDisplay();
-
 const keys = document.querySelector('.container');
-keys.addEventListener('click', (event) => {
-  const { target } = event;
+keys.addEventListener('click', event => {
+  const {
+      target
+  } = event;
+  const {
+      value
+  } = target;
   if (!target.matches('button')) {
-    return;
+      return;
   }
 
-  if (target.classList.contains('operator')) {
-    handleOperator(target.value);
-    updateDisplay();
-    return;
+  switch (value) {
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+      case '=':
+          handleOperator(value);
+          break;
+      case '.':
+          inputDecimal(value);
+          break;
+      case 'all-clear':
+          resetCalculator();
+          break;
+      case 'sign':
+          changeSign();
+          break;
+      case 'percent-of':
+          percentOf();
+          break;
+      default:
+          if (Number.isInteger(parseFloat(value))) {
+              inputDigit(value);
+          }
   }
 
-  if (target.classList.contains('decimal')) {
-    inputDecimal(target.value);
-    updateDisplay();
-    return;
-  }
-
-  if (target.classList.contains('clear')) {
-    resetCalculator();
-    updateDisplay();
-    return;
-  }
-  if (target.classList.contains('sign')) {
-    changeSign()
-    updateDisplay();
-    return;
-  }
-  if (target.classList.contains('percent')) {
-    percentOf()
-    updateDisplay()
-    return;
-  }
-
-  inputDigit(target.value);
   updateDisplay();
 });
 
 function inputDigit(digit) {
-  const { displayValue, waitingForSecondOperand } = calculator;
+  const {
+      displayValue,
+      waitingForSecondOperand
+  } = calculator;
 
   if (waitingForSecondOperand === true) {
-    calculator.displayValue = digit;
-    calculator.waitingForSecondOperand = false;
+      calculator.displayValue = digit;
+      calculator.waitingForSecondOperand = false;
   } else {
-    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+      calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
   }
 
   console.log(calculator);
 }
 
 function inputDecimal(dot) {
-  if (calculator.waitingForSecondOperand === true){
-    calculator.displayValue = '0.'
-    calculator.waitingForSecondOperand = false
-    return
+  if (calculator.waitingForSecondOperand === true) {
+      calculator.displayValue = '0.'
+      calculator.waitingForSecondOperand = false
+      return
   }
-  if (!calculator.displayValue.includes(dot)){
-    calculator.displayValue += dot;
+  if (!calculator.displayValue.includes(dot)) {
+      calculator.displayValue += dot;
   }
 }
 
 function handleOperator(nextOperator) {
-  const {firstOperand, displayValue, operator} = calculator
+  const {
+      firstOperand,
+      displayValue,
+      operator
+  } = calculator
   const inputValue = parseFloat(displayValue)
-  if (operator && calculator.waitingForSecondOperand)  {
-    calculator.operator = nextOperator;
-    console.log(calculator);
-    return;
+  if (operator && calculator.waitingForSecondOperand) {
+      calculator.operator = nextOperator;
+      console.log(calculator);
+      return;
   }
   if (firstOperand === null && !isNaN(inputValue)) {
-    calculator.firstOperand = inputValue
+      calculator.firstOperand = inputValue
   } else if (operator) {
       const result = calculate(firstOperand, inputValue, operator);
-      calculator.displayValue = String(result);
+      // fixed 0.1 + 0.2 = 0.300(0)4
+      // using toFixed() that will restrict the digits after decimal point
+      calculator.displayValue = `${parseFloat(result.toFixed(7))}`
       calculator.firstOperand = result;
   }
-  
+
   calculator.waitingForSecondOperand = true
   calculator.operator = nextOperator
   console.log(calculator);
@@ -100,13 +110,13 @@ function handleOperator(nextOperator) {
 
 function calculate(firstOperand, secondOperand, operator) {
   if (operator === '+') {
-    return firstOperand + secondOperand;
+      return firstOperand + secondOperand;
   } else if (operator === '-') {
-    return firstOperand - secondOperand;
+      return firstOperand - secondOperand;
   } else if (operator === '*') {
-    return firstOperand * secondOperand;
+      return firstOperand * secondOperand;
   } else if (operator === '/') {
-    return firstOperand / secondOperand;
+      return firstOperand / secondOperand;
   }
 
   return secondOperand;
@@ -128,5 +138,5 @@ function changeSign() {
 
 function percentOf() {
   let result = Number(calculator.displayValue)
-  calculator.displayValue = result * 1/100
+  calculator.displayValue = result * 1 / 100
 }
